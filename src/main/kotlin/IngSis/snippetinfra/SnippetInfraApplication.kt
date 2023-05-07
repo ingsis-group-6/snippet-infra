4a9dc11aa31efd99b68971e6c53a91aca2db26d0
Santiago Encina <70921727+sencina@users.noreply.github.com>
2023-05-07 16:11:59 -0300

    Add files via upload

---
+++ src/main/kotlin/IngSis/snippetinfra/SnippetInfraApplication.kt
@@ -0,0 +1,11 @@
+package IngSis.snippetinfra
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetInfraApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnippetInfraApplication>(*args)
+}
